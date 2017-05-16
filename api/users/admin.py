7703b0c60a55ee8dfff1ceada90235090f87ac10
Info-Screen <Info-Screen@Info-Screen.me>
2017-05-16 08:31:12 +0200

    Cleaned up admin.py's
    Summary:
    Deleted account/admin.py -> empty
    modified users/admin.py to use the `@admin.register()` decorator
    
    Test Plan: Used admin Interface
    
    Reviewers: WyldBot
    
    Reviewed By: WyldBot
    
    Differential Revision: https://phabricator.wildfyre.net/D18

--- api/users/admin.py
+++ api/users/admin.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 from .models import Profile
 
 
-# Register your models here.
+@admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     fields = ['user', 'avatar', 'bio', ]
     list_display = ('user', 'avatar', 'bio',)
-
-admin.site.register(Profile, ProfileAdmin)
